156700a5fd715d72e92694b0b883bae28e0f8015
Unju Shizen Nouen <unjushizennouen@gmail.com>
2025-03-16 15:31:28 +0900

    Update Home.js for deployment

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,17 +3,7 @@ import "./Home.css"; // CSSファイルをインポート
 
 export default function Home() {
   return (
-    <div className="home-container"
-      style={{
-        backgroundImage: "url('/homepage_top.jpg')",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        height: "100vh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        color: "white",
-      }}>
+    <div className="home-container">
       <h1>遠州エコビレッジ</h1>
       <Link to="/about" className="button">入口</Link>
     </div>
